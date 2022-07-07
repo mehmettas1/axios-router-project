40e1435785240d9b3e207cec4753e8f9b40afd08
mehmet <tas349873@gmail.com>
2022-07-08 00:39:57 +0300

    update

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,15 +3,25 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import About from '../pages/About'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import NotFound from '../pages/NotFound'
 import NavBar from '../components/NavBar'
 const AppRouter = () => {
   return (
-    <div>
-        <About/>
-        <Home/>
-        <Login/>
+    
+        <BrowserRouter>
         <NavBar/>
-    </div>
+        <Routes>
+            <Route path='/' element= {  <Login/>} />
+            <Route path='/about' element= { <About/>} />
+            <Route path='/home' element= { <Home/>} />
+            <Route path='*' element= { <NotFound/>} />
+       
+        
+        </Routes>
+        
+      
+        </BrowserRouter>
+    
   )
 }
 
