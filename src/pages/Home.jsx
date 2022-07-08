38565c608aae6dd1f869ff89f62175b4c8316e10
mehmet <tas349873@gmail.com>
2022-07-08 12:04:04 +0300

    searc user

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,42 @@
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import  SearchUser  from '../components/SearchUser'
+import  Followers  from '../components/SearchUser'
+
 
 const Home = () => {
+
+const [allFollowers, setAllFollowers] = useState([])
+const [loading, setLoading] = useState(true)
+const [search, setSearch] = useState("")
+
+
+const getFollowers = async()=> {
+  const {data} = await axios.get("https://api.github.com/users/mehmettas1/followers?per_page=100")
+  setAllFollowers(data)
+  console.log(data);
+}
+const handleChange =(e)=>{
+  setSearch(e.target.value)
+  console.log(e.target.value);
+}
+useEffect(()=>{
+  getFollowers()
+  setTimeout(()=>{
+    setLoading(false)
+  },3000)
+},[])
+
+const followersList = allFollowers.filter(follower=>follower.login.includes(search))
+
+
   return (
-    <div>Home</div>
+    <div>
+      <SearchUser handleChange= {handleChange}/>
+      <Followers followers = {{loading,followersList}}/>
+    </div>
   )
 }
 
