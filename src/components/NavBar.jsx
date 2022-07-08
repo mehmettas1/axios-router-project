f7c4db5e979290acdf6e2e3186ec7f59dfc39303
mehmet <tas349873@gmail.com>
2022-07-08 10:45:19 +0300

    Navbar-login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,30 +2,28 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from "react-router-dom";
+
 
 
 const NavBar = () => {
-const [isOpen, setisOpen] = React.useState(sessionStorage.getItem("email")||false)
+  const [isOpen,setIsOpen]=React.useState(sessionStorage.getItem("email") || false)
 
-const handleClick = ()=> {
-  setisOpen(false)
-  sessionStorage.clear()
-}
-
+  const handleClick=()=>{
+    setIsOpen(false)
+    sessionStorage.clear();
+  }
   return (
     <div>
-      
-
-      <Navbar bg="dark" variant="dark">
-       {isOpen ? ( <Container>
+      <Navbar bg="success" variant="dark">
+        {isOpen ? (<Container>
           <Navbar.Brand href="/home">Clarusway</Navbar.Brand>
           <Nav className="me-auto">
             <NavLink to="/home">Home</NavLink>
             <NavLink to="/about">About</NavLink>
-            <NavLink to="/" onClick={handleClick} >logout</NavLink>
+            <NavLink to="/" onClick={handleClick}>Logout</NavLink>
           </Nav>
-        </Container>) : (<Container>
+        </Container>):(<Container>
           <Navbar.Brand href="">Clarusway</Navbar.Brand></Container>)}
       </Navbar>
     </div>
